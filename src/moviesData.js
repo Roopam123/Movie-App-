1d93b94bf29eac6eeb98dac6dd01e36a9b3013f1
Roopam123 <roopamr2411@gmail.com>
2023-05-18 17:22:49 +0530

    Movie-App Using React.JS

---
+++ src/moviesData.js
@@ -0,0 +1,32 @@
+export const movies = [{
+    title: "The Avengers",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+
+    {
+    title: "Ironman",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster: "https://www.themoviedb.org/t/p/original/pD4QpqWjsC4LrfB5BkNAFLPajxv.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+    {
+    title: "The Kerala Story",
+    plot: "The Kerala Story' narrates the tales of three young girls from various regions of Kerala, with the primary focus on the story of Shalini, who is abducted and subsequently converted to Islam. Shalini is then radicalized. and compelled to join ISIS as a terrorist.",
+    price: 199,
+    rating: 8.0,
+    poster: "https://st1.bollywoodlife.com/wp-content/uploads/2023/05/kerala-3.png",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+]
